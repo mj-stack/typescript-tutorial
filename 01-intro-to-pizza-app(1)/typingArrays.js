16965c635e479fab4d45f6cb63b2b528ee667a17
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-16 23:02:23 +0530

    Add TypeScript implementation for ages and people data structure

---
+++ 01-intro-to-pizza-app(1)/typingArrays.js
@@ -0,0 +1,14 @@
+var ages = [100, 101];
+ages.push();
+var People1 = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+};
+var People2 = {
+    name: "Jill",
+    age: 66,
+    isStudent: false
+};
+var PeopleArray = [People1, People2];
+console.log(PeopleArray);
---
+++ 01-intro-to-pizza-app(1)/typingArrays.ts
@@ -0,0 +1,23 @@
+let ages: number[] = [100, 101]
+ages.push()
+
+type People = {
+  name: string,
+  age: number,
+  isStudent: boolean
+}
+
+let People1: People = {
+  name: "Joe",
+  age: 42,
+  isStudent: true
+}
+
+let People2: People = {
+  name: "Jill",
+  age: 66,
+  isStudent: false
+}
+
+let PeopleArray: People[] =[People1, People2]
+console.log(PeopleArray)
