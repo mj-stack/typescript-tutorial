4ac4a6f15ed7f694f455e9ea9bc33af058697d21
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-27 13:16:57 +0530

    Refactor code structure and remove redundant changes

--- ts-as-backend/dist/controllers/users.js
+++
@@ -1,7 +0,0 @@
-const getUsers = (req, res) => {
-    res.send([]);
-};
-const getUserById = (req, res) => {
-    res.send({});
-};
-export { getUsers, getUserById };
--- ts-as-backend/dist/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import router from "./routes/users";
-const app = express();
-app.use("/api/users", router);
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log("Server is listening at http://localhost:" + PORT);
-});
